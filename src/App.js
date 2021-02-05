a434f9af09e7bc402d01142962dd5fdb8c28630a
Muhammad Azhar <azharmuham13@hotmail.com>
2021-02-05 20:18:49 +0530

    Added tailwind css

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-
-import './App.css';
-import Header from './components/Header/Header'
-import Sidebar from './components/Sidebar/Sidebar'
+import "./App.css";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div className="app">
-      
-      <Sidebar/>
+      <Sidebar />
       <div className="app__content">
-      <Header/>
-      <Router>
-      <Switch>
-        
-        <Route path="/home">
-        <h1>This is home path</h1>
-        </Route>
-        <Route path="/">
-            <h1>This is root path</h1>
-        </Route>
-      </Switch>
-    </Router>
+        <Header />
+        <Router>
+          <Switch>
+            <Route path="/home">
+              <h1>This is home path</h1>
+            </Route>
+            <Route path="/">
+              <h1>This is root path</h1>
+            </Route>
+          </Switch>
+        </Router>
       </div>
-      
+
       {/* React Router */}
     </div>
   );
